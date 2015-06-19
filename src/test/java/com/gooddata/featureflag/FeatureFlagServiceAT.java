9fd97484ee543696916565a61cb7343fed0261ac
Libor Rysavy <libor.rysavy@gooddata.com>
2015-06-19 19:11:28 +0200

    Move getProjectFeatureFlags to FeatureFlagService
    Lets move support of feature flags of all scopes
    to its own service in its own package. Also fix
    naming conventions and use helper return objects
    instead of lists.

--- src/test/java/com/gooddata/featureflag/FeatureFlagServiceAT.java
+++ src/test/java/com/gooddata/featureflag/FeatureFlagServiceAT.java
@@ -18,6 +18,18 @@ public class FeatureFlagServiceAT extends AbstractGoodDataAT {
     private static final String PROJECT_FEATURE_FLAG = "testFeatureFlag";
 
     @Test(groups = "featureFlag", dependsOnGroups = "project")
+    public void getProjectFeatureFlags() throws Exception {
+        gd.getProjectService().createFeatureFlag(project,
+                new com.gooddata.project.ProjectFeatureFlag("mostRecentFeatureFlag"));
+
+        final ProjectFeatureFlags flags = gd.getFeatureFlagService().getProjectFeatureFlags(project);
+
+        assertThat(flags, hasItems(
+                new ProjectFeatureFlag("mostRecentFeatureFlag", true),
+                new ProjectFeatureFlag(PROJECT_FEATURE_FLAG, true)));
+    }
+
+    @Test(groups = "featureFlag", dependsOnMethods = "getProjectFeatureFlags")
     public void getFeatureFlags() throws Exception {
         final FeatureFlags flags = gd.getFeatureFlagService().getFeatureFlags(project);
 
